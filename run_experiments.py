83d59e69984445d7a0a868a3e81ca050543300ef
Mateusz Obszanski <matobszanski@student.agh.edu.pl>
2021-12-30 13:26:47 +0100

    scripts

---
+++ run_experiments.py
@@ -0,0 +1,16 @@
+from argparse import ArgumentParser
+
+import numpy as np
+
+
+def main(n):
+    rng = np.random.default_rng()
+    execute_rand_experiments_tsp(n=n, rng=rng)
+    
+    
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Run multiple experiments")
+    parser.add_argument("--n", help="no. of experiments to run")
+    args = parser.parse_args()
+    
+    main(args.n)
